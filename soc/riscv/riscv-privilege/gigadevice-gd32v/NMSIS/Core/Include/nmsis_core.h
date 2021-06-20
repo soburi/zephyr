5960e397a46a49d7cedfe6dab66356e5fc90432f
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2021-06-21 01:08:41 +0900

    soc: remove dependency on nuclei external module
    Use common CSR definitions defined in zephyr.
    Define Nuclie specific CSR in soc.h.
    Incorporate core_feature_eclic.h.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

---
+++ soc/riscv/riscv-privilege/gigadevice-gd32v/NMSIS/Core/Include/nmsis_core.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2021 Tokita, Hiroshi <tokita.hiroshi@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __NMSIS_CORE_H__
+#define __NMSIS_CORE_H__
+
+/** \brief Define a static function that should be always inlined by the compiler. */
+#ifndef   __STATIC_FORCEINLINE
+  #define __STATIC_FORCEINLINE                   __attribute__((always_inline)) static inline
+#endif
+
+/** \brief Defines 'read only' permissions */
+#ifdef __cplusplus
+  #define   __I     volatile
+#else
+  #define   __I     volatile const
+#endif
+/** \brief Defines 'write only' permissions */
+#define     __O     volatile
+/** \brief Defines 'read / write' permissions */
+#define     __IO    volatile
+
+/* following defines should be used for structure members */
+/** \brief Defines 'read only' structure member permissions */
+#define     __IM     volatile const
+/** \brief Defines 'write only' structure member permissions */
+#define     __OM     volatile
+/** \brief Defines 'read/write' structure member permissions */
+#define     __IOM    volatile
+
+#ifndef __RISCV_XLEN
+/** \brief Refer to the width of an integer register in bits(either 32 or 64) */
+  #ifndef __riscv_xlen
+    #define __RISCV_XLEN    32
+  #else
+    #define __RISCV_XLEN    __riscv_xlen
+  #endif
+#endif /* __RISCV_XLEN */
+
+/** \brief Type of Control and Status Register(CSR), depends on the XLEN defined in RISC-V */
+#if __RISCV_XLEN == 32
+typedef uint32_t rv_csr_t;
+#elif __RISCV_XLEN == 64
+typedef uint64_t rv_csr_t;
+#else
+typedef uint32_t rv_csr_t;
+#endif
+
+#define __RV_CSR_READ(x) csr_read(x)
+#define __RV_CSR_WRITE(x, y) csr_write(x, y)
+
+#include <core_feature_eclic.h>
+
+#endif
--- soc/riscv/riscv-privilege/gigadevice-gd32v/soc.h
+++ soc/riscv/riscv-privilege/gigadevice-gd32v/soc.h
@@ -13,6 +13,7 @@
 
 #include <soc_common.h>
 #include <devicetree.h>
+#include <arch/riscv/csr.h>
 
 /* Timer configuration */
 #define RISCV_MTIME_BASE             (0xD1000000UL + 0)
@@ -21,6 +22,44 @@
 /* lib-c hooks required RAM defined variables */
 #define RISCV_RAM_BASE               DT_SRAM_BASE_ADDRESS
 #define RISCV_RAM_SIZE               KB(DT_SRAM_SIZE)
+
+/* === CLIC CSR Registers === */
+#define CSR_MTVT                0x307
+#define CSR_MNXTI               0x345
+#define CSR_MINTSTATUS          0x346
+#define CSR_MSCRATCHCSW         0x348
+#define CSR_MSCRATCHCSWL        0x349
+#define CSR_MCLICBASE           0x350
+
+/* === Nuclei custom CSR Registers === */
+#define CSR_MCOUNTINHIBIT       0x320
+#define CSR_MILM_CTL            0x7C0
+#define CSR_MDLM_CTL            0x7C1
+#define CSR_MNVEC               0x7C3
+#define CSR_MSUBM               0x7C4
+#define CSR_MDCAUSE             0x7C9
+#define CSR_MCACHE_CTL          0x7CA
+#define CSR_MMISC_CTL           0x7D0
+#define CSR_MSAVESTATUS         0x7D6
+#define CSR_MSAVEEPC1           0x7D7
+#define CSR_MSAVECAUSE1         0x7D8
+#define CSR_MSAVEEPC2           0x7D9
+#define CSR_MSAVECAUSE2         0x7DA
+#define CSR_MSAVEDCAUSE1        0x7DB
+#define CSR_MSAVEDCAUSE2        0x7DC
+#define CSR_PUSHMSUBM           0x7EB
+#define CSR_MTVT2               0x7EC
+#define CSR_JALMNXTI            0x7ED
+#define CSR_PUSHMCAUSE          0x7EE
+#define CSR_PUSHMEPC            0x7EF
+#define CSR_MPPICFG_INFO        0x7F0
+#define CSR_MFIOCFG_INFO        0x7F1
+#define CSR_SLEEPVALUE          0x811
+#define CSR_TXEVT               0x812
+#define CSR_WFE                 0x810
+#define CSR_MICFG_INFO          0xFC0
+#define CSR_MDCFG_INFO          0xFC1
+#define CSR_MCFG_INFO           0xFC2
 
 #ifndef _ASMLANGUAGE
 #include <toolchain.h>
