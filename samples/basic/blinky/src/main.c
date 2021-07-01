af4714eb51fcfda82ae6a98b568d615965c92a23
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2021-07-01 19:38:29 +0900

    interrupt raised

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -26,7 +26,32 @@
 #define PIN	0
 #define FLAGS	0
 #endif
-#define csr_set set_csr
+//#include <init.h>
+//#include <toolchain.h>
+#include <soc.h>
+#include <arch/riscv/csr.h>
+//#define BIT(x)                       ((uint32_t)((uint32_t)0x01U<<(x)))
+//#define GPIO_BASE             (APB2_BUS_BASE + 0x00000800U)  /*!< GPIO base address                */
+//#define RCU_BASE              (AHB1_BUS_BASE + 0x00009000U)  /*!< RCU base address                 */
+//#define AFIO_BASE             (APB2_BUS_BASE + 0x00000000U)  /*!< AFIO base address                */
+//#define EXTI_BASE             (APB2_BUS_BASE + 0x00000400U)  /*!< EXTI base address                */
+//#define csr_set set_csr
+//extern void delay_1ms(uint32_t);
+//static void k_msleep(uint32_t x) { delay_1ms(x); }
+//#define __SYSTEM_CLOCK_108M_PLL_HXTAL           (uint32_t)(108000000)
+/* define interrupt number */
+//typedef enum IRQn
+//{
+//    EXTI10_15_IRQn               = 59,     /*!< EXTI[15:10] interrupts                                   */
+//} IRQn_Type;
+
+/* peripheral memory map */
+#define APB1_BUS_BASE         ((uint32_t)0x40000000U)        /*!< apb1 base address                */
+#define APB2_BUS_BASE         ((uint32_t)0x40010000U)        /*!< apb2 base address                */
+#define AHB1_BUS_BASE         ((uint32_t)0x40018000U)        /*!< ahb1 base address                */
+#define AHB3_BUS_BASE         ((uint32_t)0x60000000U)        /*!< ahb3 base address                */
+
+
 
 /* RCU definitions */
 #define RCU                             RCU_BASE
@@ -97,6 +122,10 @@
 #define CFG0_REG_OFFSET                 0x04U                     /*!< clock configuration register 0 offset */
 #define CFG1_REG_OFFSET                 0x2CU                     /*!< clock configuration register 1 offset */
 
+/* GPIO mode values set */
+#define GPIO_MODE_SET(n, mode)           ((uint32_t)((uint32_t)(mode) << (4U * (n))))
+#define GPIO_MODE_MASK(n)                (0xFU << (4U * (n)))
+
 const uint32_t	RCU_GPIOC = RCU_REGIDX_BIT(APB2EN_REG_OFFSET, 4U);
 
 void __rcu_periph_clock_enable(uint32_t periph)
@@ -104,10 +133,205 @@ void __rcu_periph_clock_enable(uint32_t periph)
     RCU_REG_VAL(periph) |= BIT(RCU_BIT_POS(periph));
 }
 
-/* GPIO mode values set */
-#define GPIO_MODE_SET(n, mode)           ((uint32_t)((uint32_t)(mode) << (4U * (n))))
-#define GPIO_MODE_MASK(n)                (0xFU << (4U * (n)))
+struct gd32_afio {
+	volatile uint32_t EC;
+	volatile uint32_t PCF0;
+	volatile uint32_t EXTISS0;
+	volatile uint32_t EXTISS1;
+	volatile uint32_t EXTISS2;
+	volatile uint32_t EXTISS3;
+	volatile uint32_t PCF1;
+};
 
+
+struct gd32_exti {
+	volatile uint32_t INTEN;
+	volatile uint32_t EVEN;
+	volatile uint32_t RTEN;
+	volatile uint32_t FTEN;
+	volatile uint32_t SWIEV;
+	volatile uint32_t PD;
+};
+
+
+#define AFIO ((volatile struct gd32_afio *)(AFIO_BASE))
+#define AFIO_EXTI_PORT_MASK     ((uint8_t)0x70)
+#define AFIO_EXTI_SOURCE_FIELDS ((uint8_t)0x04U) /*!< select AFIO exti source registers */
+#define AFIO_EXTI_SOURCE_MASK   ((uint8_t)0x03U)
+
+
+
+#define GPIO_PIN_SOURCE(x) ((uint32_t)x)
+
+
+#define AFIO ((volatile struct gd32_afio *)(AFIO_BASE))
+
+
+#define AFIO_EXTI_PORT_MASK     ((uint8_t)0x70)
+#define AFIO_EXTI_SOURCE_FIELDS ((uint8_t)0x04U) /*!< select AFIO exti source registers */
+#define AFIO_EXTI_SOURCE_MASK   ((uint8_t)0x03U)
+
+#define EXTI ((volatile struct gd32_exti *)(EXTI_BASE))
+
+#define ECLIC_ADDR_BASE           0xd2000000
+//  0x1000+4*i   1B/input    RW        eclicintip[i]
+#define ECLIC_INT_IP_OFFSET            0x1000UL
+//  0x1001+4*i   1B/input    RW        eclicintie[i]
+#define ECLIC_INT_IE_OFFSET            0x1001UL
+//  0x1002+4*i   1B/input    RW        eclicintattr[i]
+#define ECLIC_INT_ATTR_OFFSET          0x1002UL
+#define ECLICINTCTLBITS  4
+#define ECLIC_CFG_NLBITS_LSB     (1u)
+#define ECLIC_CFG_NLBITS_MASK          0x1EUL
+#define ECLIC_CFG_OFFSET            0x0
+
+//  0x1003+4*i   1B/input    RW        eclicintctl[i]
+#define ECLIC_INT_CTRL_OFFSET          0x1003UL
+
+void __eclic_enable_interrupt (uint32_t source) {
+    *(volatile uint8_t*)(ECLIC_ADDR_BASE+ECLIC_INT_IE_OFFSET+source*4) = 1;
+}
+
+void __eclic_set_intctrl (uint32_t source, uint8_t intctrl){
+  *(volatile uint8_t*)(ECLIC_ADDR_BASE+ECLIC_INT_CTRL_OFFSET+source*4) = intctrl;
+}
+
+uint8_t __eclic_get_intctrl  (uint32_t source){
+  return *(volatile uint8_t*)(ECLIC_ADDR_BASE+ECLIC_INT_CTRL_OFFSET+source*4);
+}
+
+void __eclic_set_cliccfg (uint8_t cliccfg){
+  *(volatile uint8_t*)(ECLIC_ADDR_BASE+ECLIC_CFG_OFFSET) = cliccfg;
+}
+
+uint8_t __eclic_get_cliccfg  (){
+  return *(volatile uint8_t*)(ECLIC_ADDR_BASE+ECLIC_CFG_OFFSET);
+}
+//sets nlbits 
+void __eclic_set_nlbits(uint8_t nlbits) {
+  //shift nlbits to correct position
+  uint8_t nlbits_shifted = nlbits << ECLIC_CFG_NLBITS_LSB;
+
+  //read the current cliccfg 
+  uint8_t old_cliccfg = __eclic_get_cliccfg();
+  uint8_t new_cliccfg = (old_cliccfg & (~ECLIC_CFG_NLBITS_MASK)) | (ECLIC_CFG_NLBITS_MASK & nlbits_shifted); 
+
+  __eclic_set_cliccfg(new_cliccfg);
+}
+
+//get nlbits 
+uint8_t __eclic_get_nlbits(void) {
+  //extract nlbits
+  uint8_t nlbits = __eclic_get_cliccfg();
+  nlbits = (nlbits & ECLIC_CFG_NLBITS_MASK) >> ECLIC_CFG_NLBITS_LSB;
+  return nlbits;
+}
+
+//sets an interrupt priority based encoding of nlbits and ECLICINTCTLBITS
+uint8_t __eclic_set_irq_priority(uint32_t source, uint8_t priority) {
+	//extract nlbits
+	uint8_t nlbits = __eclic_get_nlbits();
+	if (nlbits >= ECLICINTCTLBITS) {
+		nlbits = ECLICINTCTLBITS;
+		return 0;
+	}
+
+	//shift priority into correct bit position
+	priority = priority << (8 - ECLICINTCTLBITS);
+
+	//write to eclicintctrl
+	uint8_t current_intctrl = __eclic_get_intctrl(source);
+	//shift intctrl right to mask off unused bits
+	current_intctrl = current_intctrl >> (8-nlbits);
+	//shift intctrl into correct bit position
+	current_intctrl = current_intctrl << (8-nlbits);
+
+	__eclic_set_intctrl(source, (current_intctrl | priority));
+
+	return priority;
+}
+
+void __eclic_set_irq_lvl_abs(uint32_t source, uint8_t lvl_abs) {
+  //extract nlbits
+  uint8_t nlbits = __eclic_get_nlbits();
+  if (nlbits > ECLICINTCTLBITS) {
+    nlbits = ECLICINTCTLBITS;
+  }
+
+  //shift lvl_abs into correct bit position
+  uint8_t lvl = lvl_abs << (8-nlbits);
+ 
+  //write to clicintctrl
+  uint8_t current_intctrl = __eclic_get_intctrl(source);
+  //shift intctrl left to mask off unused bits
+  current_intctrl = current_intctrl << nlbits;
+  //shift intctrl into correct bit position
+  current_intctrl = current_intctrl >> nlbits;
+
+  __eclic_set_intctrl(source, (current_intctrl | lvl));
+}
+
+void exti_source_select(uint8_t output_port, uint8_t output_pin)
+{
+    uint32_t source = 0U;
+    source = ((uint32_t) 0x0FU)
+            << (AFIO_EXTI_SOURCE_FIELDS * (output_pin & AFIO_EXTI_SOURCE_MASK));
+
+    /* select EXTI sources */
+    if (GPIO_PIN_SOURCE(4) > output_pin) {
+        /* select EXTI0/EXTI1/EXTI2/EXTI3 */
+        AFIO->EXTISS0 &= ~source;
+        AFIO->EXTISS0 |= (((uint32_t) output_port)
+                << (AFIO_EXTI_SOURCE_FIELDS
+                        * (output_pin & AFIO_EXTI_SOURCE_MASK)));
+    } else if (GPIO_PIN_SOURCE(8) > output_pin) {
+        /* select EXTI4/EXTI5/EXTI6/EXTI7 */
+        AFIO->EXTISS1 &= ~source;
+        AFIO->EXTISS1 |= (((uint32_t) output_port)
+                << (AFIO_EXTI_SOURCE_FIELDS
+                        * (output_pin & AFIO_EXTI_SOURCE_MASK)));
+    } else if (GPIO_PIN_SOURCE(12) > output_pin) {
+        /* select EXTI8/EXTI9/EXTI10/EXTI11 */
+        AFIO->EXTISS2 &= ~source;
+        AFIO->EXTISS2 |= (((uint32_t) output_port)
+                << (AFIO_EXTI_SOURCE_FIELDS
+                        * (output_pin & AFIO_EXTI_SOURCE_MASK)));
+    } else {
+        /* select EXTI12/EXTI13/EXTI14/EXTI15 */
+        AFIO->EXTISS3 &= ~source;
+        AFIO->EXTISS3 |= (((uint32_t) output_port)
+                << (AFIO_EXTI_SOURCE_FIELDS
+                        * (output_pin & AFIO_EXTI_SOURCE_MASK)));
+    }
+}
+
+void init()
+{
+	//Enable the interrupts. From now on interrupt handlers can be executed
+	//eclic_global_interrupt_enable();	
+    csr_set(mstatus, MSTATUS_MIE);
+    __eclic_set_nlbits(ECLIC_PRIGROUP_LEVEL4_PRIO0);
+
+	exti_source_select(GPIO_PORT_SOURCE_GPIOC, GPIO_PIN_SOURCE_13);
+	//exti_init(EXTI_13, EXTI_INTERRUPT, EXTI_TRIG_BOTH);
+
+    /* reset the EXTI line x */
+    EXTI->EVEN  &= ~BIT(13);
+    EXTI->INTEN &= ~BIT(13);
+    EXTI->INTEN |= BIT(13);
+    
+    EXTI->RTEN |= BIT(13);
+    EXTI->FTEN |= BIT(13);
+
+    EXTI->PD = BIT(13);
+
+
+    __eclic_enable_interrupt(EXTI10_15_IRQn);
+    __eclic_set_irq_lvl_abs(EXTI10_15_IRQn, 1);
+    __eclic_set_irq_priority(EXTI10_15_IRQn, 1);
+
+	return;
+}
 
 
 void __gpio_init(uint32_t gpio_periph, uint32_t mode, uint32_t speed,
@@ -185,35 +409,7 @@ void longan_led_init()
 
     GPIO_BC(LED_GPIO_PORT) = LED_PIN;
 }
-#if 0
-void init()
-{
-	//Enable the interrupts. From now on interrupt handlers can be executed
-	//eclic_global_interrupt_enable();	
-    csr_set(mstatus, MSTATUS_MIE);
-    __eclic_set_nlbits(ECLIC_PRIGROUP_LEVEL4_PRIO0);
 
-	exti_source_select(GPIO_PORT_SOURCE_GPIOC, GPIO_PIN_SOURCE_13);
-	//exti_init(EXTI_13, EXTI_INTERRUPT, EXTI_TRIG_BOTH);
-
-    /* reset the EXTI line x */
-    EXTI->EVEN  &= ~BIT(13);
-    EXTI->INTEN &= ~BIT(13);
-    EXTI->INTEN |= BIT(13);
-    
-    EXTI->RTEN |= BIT(13);
-    EXTI->FTEN |= BIT(13);
-
-    EXTI->PD = BIT(13);
-
-
-    __eclic_enable_interrupt(EXTI10_15_IRQn);
-    __eclic_set_irq_lvl_abs(EXTI10_15_IRQn, 1);
-    __eclic_set_irq_priority(EXTI10_15_IRQn, 1);
-
-	return;
-}
-#endif
 void longan_led_on()
 {
     /*
@@ -235,6 +431,7 @@ void main(void)
 	int ret;
 
 	longan_led_init();
+	init();
 	//dev = device_get_binding(LED0);
 	//if (dev == NULL) {
 	//	return;
--- soc/riscv/riscv-privilege/gigadevice-gd32v/soc.c
+++ soc/riscv/riscv-privilege/gigadevice-gd32v/soc.c
@@ -9,9 +9,19 @@
 #include <stdio.h>
 #include <init.h>
 
+static void __gd32_exti_isr_10_15(const void *arg)
+{
+	//__gd32_exti_isr(10, 16, arg);
+}
+
 static int _init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+
+	IRQ_CONNECT(EXTI10_15_IRQn,
+		    0,
+		    __gd32_exti_isr_10_15, NULL,
+		    0);
 
 	/* Enable mcycle and minstret counter */
 	__asm__ ("csrsi mcountinhibit, 0x5");
