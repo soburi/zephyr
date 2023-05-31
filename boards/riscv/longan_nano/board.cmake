e8d505081c9b3cebfdb85bbc60a070ebecbc9f54
TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
2023-05-31 20:54:26 +0900

    openocd

--- boards/riscv/longan_nano/board.cmake
+++ boards/riscv/longan_nano/board.cmake
@@ -1,7 +1,14 @@
 # Copyright (c) 2021 Tokita, Hiroshi <tokita.hiroshi@gmail.com>
 # SPDX-License-Identifier: Apache-2.0
 
+if ("${LONGAN_NANO_DEBUG_ADAPTER}" STREQUAL "")
+	set(LONGAN_NANO_DEBUG_ADAPTER um232h)
+endif()
+
+board_runner_args(openocd --cmd-pre-init "source [find interface/ftdi/${LONGAN_NANO_DEBUG_ADAPTER}.cfg]")
 board_runner_args(openocd --cmd-pre-init "source [find target/gd32vf103.cfg]")
+board_runner_args(openocd --cmd-pre-init "adapter speed 2000")
+
 
 board_runner_args(openocd "--cmd-pre-load=gd32vf103-pre-load")
 board_runner_args(openocd "--cmd-load=gd32vf103-load")
