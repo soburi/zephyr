2a67aea67547ea4e5c731a986144c30d46670740
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2023-09-29 08:25:55 +0900

    x

--- jam_demo/src/main.c
+++ jam_demo/src/main.c
@@ -15,10 +15,38 @@
 #include "posix_board_if.h"
 #endif
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(u8g2_logo, LOG_LEVEL_INF);
+#include <errno.h>
+#include <string.h>
 
-static void drawLogo(u8g2_t *u8g2)
+#define LOG_LEVEL 4
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
+
+#define STRIP_NODE		DT_ALIAS(led_strip)
+#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+
+#define DELAY_TIME K_MSEC(50)
+
+#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
+
+static const struct led_rgb colors[] = {
+	RGB(0x0f, 0x00, 0x00), /* red */
+	RGB(0x00, 0x0f, 0x00), /* green */
+	RGB(0x00, 0x00, 0x0f), /* blue */
+};
+
+struct led_rgb pixels[STRIP_NUM_PIXELS];
+
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
+
+
+static void drawLogo(u8g2_t *u8g2, int y)
 {
 	uint8_t mdy = 0;
 
@@ -32,53 +60,53 @@ static void drawLogo(u8g2_t *u8g2)
 
 	u8g2_SetFontDirection(u8g2, 0);
 	u8g2_SetFont(u8g2, u8g2_font_inb16_mf);
-	u8g2_DrawStr(u8g2, 0, 22, "U");
+	u8g2_DrawStr(u8g2, 0, 22 + y, "U");
 
 	u8g2_SetFontDirection(u8g2, 1);
 	u8g2_SetFont(u8g2, u8g2_font_inb19_mn);
-	u8g2_DrawStr(u8g2, 14, 8, "8");
+	u8g2_DrawStr(u8g2, 14, 8 + y, "8");
 
 	u8g2_SetFontDirection(u8g2, 0);
 	u8g2_SetFont(u8g2, u8g2_font_inb16_mf);
-	u8g2_DrawStr(u8g2, 36, 22, "g");
-	u8g2_DrawStr(u8g2, 48, 22, "\xb2");
+	u8g2_DrawStr(u8g2, 36, 22 + y, "g");
+	u8g2_DrawStr(u8g2, 48, 22 + y, "\xb2");
 
-	u8g2_DrawHLine(u8g2, 2, 25, 34);
-	u8g2_DrawHLine(u8g2, 3, 26, 34);
-	u8g2_DrawVLine(u8g2, 32, 22, 12);
-	u8g2_DrawVLine(u8g2, 33, 23, 12);
+	u8g2_DrawHLine(u8g2, 2, 25 + y, 34);
+	u8g2_DrawHLine(u8g2, 3, 26 + y, 34);
+	u8g2_DrawVLine(u8g2, 32, 22 + y, 12);
+	u8g2_DrawVLine(u8g2, 33, 23 + y, 12);
 #else
 
 	u8g2_SetFontDirection(u8g2, 0);
 	u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
-	u8g2_DrawStr(u8g2, 0, 30 - mdy, "U");
+	u8g2_DrawStr(u8g2, 0, 30 - mdy + y, "U");
 
 	u8g2_SetFontDirection(u8g2, 1);
 	u8g2_SetFont(u8g2, u8g2_font_inb30_mn);
-	u8g2_DrawStr(u8g2, 21, 8 - mdy, "8");
+	u8g2_DrawStr(u8g2, 21, 8 - mdy + y, "8");
 
 	u8g2_SetFontDirection(u8g2, 0);
 	u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
-	u8g2_DrawStr(u8g2, 51, 30 - mdy, "g");
-	u8g2_DrawStr(u8g2, 67, 30 - mdy, "\xb2");
+	u8g2_DrawStr(u8g2, 51, 30 - mdy + y, "g");
+	u8g2_DrawStr(u8g2, 67, 30 - mdy + y, "\xb2");
 
-	u8g2_DrawHLine(u8g2, 2, 35 - mdy, 47);
-	u8g2_DrawHLine(u8g2, 3, 36 - mdy, 47);
-	u8g2_DrawVLine(u8g2, 45, 32 - mdy, 12);
-	u8g2_DrawVLine(u8g2, 46, 33 - mdy, 12);
+	u8g2_DrawHLine(u8g2, 2, 35 - mdy + y, 47);
+	u8g2_DrawHLine(u8g2, 3, 36 - mdy + y, 47);
+	u8g2_DrawVLine(u8g2, 45, 32 - mdy + y, 12);
+	u8g2_DrawVLine(u8g2, 46, 33 - mdy + y, 12);
 
 #endif
 }
 
-void drawURL(u8g2_t *u8g2)
+void drawURL(u8g2_t *u8g2, int y)
 {
 #ifndef MINI_LOGO
 	u8g2_SetFont(u8g2, u8g2_font_4x6_tr);
 	if (u8g2_GetDisplayHeight(u8g2) < 59) {
-		u8g2_DrawStr(u8g2, 89, 20 - 5, "github.com");
-		u8g2_DrawStr(u8g2, 8973, 29 - 5, "/olikraus/u8g2");
+		u8g2_DrawStr(u8g2, 89, 20 - 5 + y, "github.com");
+		u8g2_DrawStr(u8g2, 8973, 29 - 5 + y, "/olikraus/u8g2");
 	} else {
-		u8g2_DrawStr(u8g2, 1, 54, "github.com/olikraus/u8g2");
+		u8g2_DrawStr(u8g2, 1, 54 + y, "github.com/olikraus/u8g2");
 	}
 #endif
 }
@@ -87,6 +115,19 @@ int main(void)
 {
 	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	u8g2_t *u8g2;
+	int count = 0;
+
+	size_t cursor = 0, color = 0;
+	int rc;
+
+	if (device_is_ready(strip)) {
+		LOG_INF("Found LED strip device %s", strip->name);
+	} else {
+		LOG_ERR("LED strip device %s is not ready", strip->name);
+		return 0;
+	}
+
+	LOG_INF("Displaying pattern on strip");
 
 	if (!device_is_ready(dev)) {
 		LOG_ERR("Device %s not found. Aborting sample.", dev->name);
@@ -114,10 +155,35 @@ int main(void)
 	u8g2_ClearBuffer(u8g2);
 
 	u8g2_FirstPage(u8g2);
-	do {
-		drawLogo(u8g2);
-		drawURL(u8g2);
-	} while (u8g2_NextPage(u8g2));
+
+	while(1) {
+		memset(&pixels, 0x00, sizeof(pixels));
+		memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
+		rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+
+		if (rc) {
+			LOG_ERR("couldn't update strip: %d", rc);
+		}
+
+		cursor++;
+		if (cursor >= STRIP_NUM_PIXELS) {
+			cursor = 0;
+			color++;
+			if (color == ARRAY_SIZE(colors)) {
+				color = 0;
+			}
+		}
+
+		u8g2_ClearBuffer(u8g2);
+		do {
+			drawLogo(u8g2, count%120);
+			drawURL(u8g2, count%120);
+		} while (u8g2_NextPage(u8g2));
+		k_sleep(DELAY_TIME);
+
+		count++;
+	}
+
 	k_sleep(K_MSEC(4000));
 
 #ifdef CONFIG_ARCH_POSIX
