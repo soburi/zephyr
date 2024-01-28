fb9ef5306b5164eeb7b9d15c58c94e035e82c7d3
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2024-01-28 23:02:06 +0900

    fb: cfb: Supporting multiple display
    There are three major changes.
    
    - Introducing `struct cfb_display`
    - Changed the argument `struct device *` to `struct cfb_framebuffer *`
    - Rename APIs starting with `cfb_framebuffer_*`
    
    Introduces struct cfb_display as a data structure to represent the display.
    As a result, it is now possible to create multiple pieces of data managed
    using a single static variable in previous implementations,
    making it possible to handle multiple displays.
    The old `struct cfb_framebuffer` remains but is now part of the
    `struct cfb_display` data structure.
    You can get cfb_framebuffer from cfb_display using the added API
    `cfb_display_get_framebuffer`.
    
    All places where a `struct device *` argument was passed to specify the
    target will be replaced with a `struct cfb_framebuffer *`.
    In line with this, API names starting with `cfb_framebuffer_*`,
    which were redundant and inconsistent names, will remove
    `_framebuffer` and change to `cfb_*` .
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- samples/subsys/display/cfb_custom_font/src/main.c
+++ samples/subsys/display/cfb_custom_font/src/main.c
@@ -11,9 +11,18 @@
 
 #include "cfb_font_dice.h"
 
+static uint8_t transfer_buffer[DIV_ROUND_UP(
+	DT_PROP(DT_CHOSEN(zephyr_display), width) * DT_PROP(DT_CHOSEN(zephyr_display), height), 8)];
+
 int main(void)
 {
 	const struct device *const display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	struct cfb_display_init_param param = {
+		.dev = display,
+		.transfer_buf = transfer_buffer,
+		.transfer_buf_size = sizeof(transfer_buffer),
+	};
+	struct cfb_display disp;
 	int err;
 
 	if (!device_is_ready(display)) {
@@ -32,22 +41,22 @@ int main(void)
 		return 0;
 	}
 
-	err = cfb_framebuffer_init(display);
+	err = cfb_display_init(&disp, &param);
 	if (err) {
 		printk("Could not initialize framebuffer (err %d)\n", err);
 	}
 
-	err = cfb_framebuffer_clear(display, true);
+	err = cfb_clear(&disp.fb, true);
 	if (err) {
 		printk("Could not clear framebuffer (err %d)\n", err);
 	}
 
-	err = cfb_print(display, "123456", 0, 0);
+	err = cfb_print(&disp.fb, "123456", 0, 0);
 	if (err) {
 		printk("Could not display custom font (err %d)\n", err);
 	}
 
-	err = cfb_framebuffer_finalize(display);
+	err = cfb_finalize(&disp.fb);
 	if (err) {
 		printk("Could not finalize framebuffer (err %d)\n", err);
 	}
