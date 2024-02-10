8d55d914d084b47061e8160a0ccd9a2d6597303a
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2024-02-10 09:54:15 +0200

    drivers: tee: optee: Use arch_page_phys_get for address conversion
    According to the description of z_mem_phys_addr it uses
    Z_MEM_VM_OFFSET to make a conversion from virtual to physical
    address. And these macros are intended for assembly, linker code,
    and static initializers.
    So when external libraries, such as PICOLIBC or NEWLIB_LIBC
    uses internal mappings for dynamic memory allocation z_mem_phys_addr
    may not work properly and return wrong physical address.
    
    We encountered this problem while testing the generation of a
    certificate using PKCS11 in libckteec with MBEDTLS.
    Mbedtls allocates buffer dynamically and ivokes tee command to request
    certificate from PKCS11 TA which should place result to the allocated
    buffer.
    The result is the MBEDTLS is used with minimal libc - then everything
    works fine. But when CONFIG_PICOLIBC or CONFIG_NEWLIB_LIBC is enabled
    - it returns empty buffer. This happens because z_mem_phys_addr
    returns incorrect physical address.
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- drivers/tee/optee/optee.c
+++ drivers/tee/optee/optee.c
@@ -218,7 +218,7 @@ static void msg_param_to_tmp_mem(struct tee_param *p, uint32_t attr,
 		return;
 	}
 
-	p->a = mp->u.tmem.buf_ptr - k_mem_phys_addr(shm->addr);
+	p->a = mp->u.tmem.buf_ptr - optee_mem_phys_addr(shm->addr);
 	p->c = mp->u.tmem.shm_ref;
 }
 
@@ -701,7 +701,8 @@ static void handle_rpc_call(const struct device *dev, struct optee_rpc_param *pa
 		if (!tee_add_shm(dev, NULL, OPTEE_MSG_NONCONTIG_PAGE_SIZE,
 				 param->a1,
 				 TEE_SHM_ALLOC, &shm)) {
-			u64_to_regs((uint64_t)k_mem_phys_addr(shm->addr), &param->a1, &param->a2);
+			u64_to_regs((uint64_t)optee_mem_phys_addr(shm->addr), &param->a1,
+				    &param->a2);
 			u64_to_regs((uint64_t)shm, &param->a4, &param->a5);
 		} else {
 			param->a1 = 0;
@@ -741,7 +742,7 @@ static int optee_call(const struct device *dev, struct optee_msg_arg *arg, struc
 	unsigned int rpc_arg_offs;
 	void *pages = NULL;
 
-	u64_to_regs((uint64_t)k_mem_phys_addr(arg), &param.a1, &param.a2);
+	u64_to_regs((uint64_t)optee_mem_phys_addr(arg), &param.a1, &param.a2);
 
 	k_sem_take(&data->call_sem, K_FOREVER);
 
@@ -1090,7 +1091,7 @@ static void *optee_construct_page_list(void *buf, uint32_t len, uint64_t *phys_b
 
 	for (uint32_t pl_idx = 0; pl_idx < list_size / page_size; pl_idx++) {
 		for (uint32_t page_idx = 0; num_pages && page_idx < num_pages_in_pl; page_idx++) {
-			pl[pl_idx].pages[page_idx] = k_mem_phys_addr(buf_page);
+			pl[pl_idx].pages[page_idx] = optee_mem_phys_addr(buf_page);
 			buf_page += page_size;
 			num_pages--;
 		}
@@ -1099,13 +1100,13 @@ static void *optee_construct_page_list(void *buf, uint32_t len, uint64_t *phys_b
 			break;
 		}
 
-		pl[pl_idx].next_page = k_mem_phys_addr(pl + 1);
+		pl[pl_idx].next_page = optee_mem_phys_addr(pl + 1);
 	}
 
 	/* 12 least significant bits of optee_msg_param.u.tmem.buf_ptr should hold page offset
 	 * of user buffer
 	 */
-	*phys_buf = k_mem_phys_addr(pl) | page_offset;
+	*phys_buf = optee_mem_phys_addr(pl) | page_offset;
 
 	return pl;
 }
