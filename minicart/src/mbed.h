bef0942931bdf516a1431540d2eca43ea56ceb69
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2024-02-17 18:19:41 +0900

    minicart

---
+++ minicart/src/mbed.h
@@ -0,0 +1,132 @@
+#pragma once
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/pwm.h>
+class DigitalOut
+{
+	const struct gpio_dt_spec &dt;
+public:
+	DigitalOut(const struct gpio_dt_spec &pdt) : dt(pdt)
+	{
+		gpio_pin_configure_dt(&dt, GPIO_OUTPUT_ACTIVE);
+	}
+	void operator=(int v)
+	{
+		gpio_pin_set_dt(&dt, v);
+	}
+};
+class DigitalIn
+{
+	const struct gpio_dt_spec &dt;
+public:
+	DigitalIn(const struct gpio_dt_spec &pdt) : dt(pdt)
+	{
+		gpio_pin_configure_dt(&dt, GPIO_INPUT);
+	}
+	bool operator==(const int &x)
+	{
+		return ((int)this == x);
+	}
+	operator int()
+	{
+		return gpio_pin_get_dt(&dt);
+	}
+};
+class InterruptIn
+{
+	const struct gpio_dt_spec &dt;
+public:
+	InterruptIn(const struct gpio_dt_spec &pdt) : dt(pdt)
+	{
+	}
+	void rise(void (*fn)())
+	{
+	}
+	void fall(void (*fn)())
+	{
+	}
+};
+class PwmOut
+{
+	const struct pwm_dt_spec &dt;
+	int period_usec;
+public:
+	PwmOut(const struct pwm_dt_spec &pdt) : dt(pdt)
+	{
+	}
+	void write(float duty)
+	{
+		pwm_set_dt(&dt, period_usec * 1000, period_usec * 1000 * duty);
+	}
+	void period_us(int us)
+	{
+		period_usec = us;
+	}
+};
+class AnalogIn
+{
+	const struct adc_dt_spec &dt;
+public:
+	AnalogIn(const struct adc_dt_spec &pdt) : dt(pdt)
+	{
+	}
+	float read()
+	{
+		uint16_t buf;
+		struct adc_sequence seq = {
+			.buffer = &buf,
+			/* buffer size in bytes, not number of samples */
+			.buffer_size = sizeof(buf),
+		};
+		(void)adc_sequence_init_dt(&dt, &seq);
+		adc_read_dt(&dt, &seq);
+		return 1.0f * buf / (2 << seq.resolution);
+	}
+};
+class Timer
+{
+	const struct device *dev;
+public:
+	Timer(const struct device *d) : dev(d)
+	{
+	}
+	void start()
+	{
+	}
+	int read_us()
+	{
+		return 0;
+	}
+};
+class Ticker
+{
+	const struct device *dev;
+public:
+	Ticker(const struct device *d) : dev(d)
+	{
+	}
+	void attach_us(void (*fn)(), float)
+	{
+	}
+};
+
+static inline void wait_ms(int x)
+{
+	k_sleep(K_MSEC(x));
+}
+
+static const struct gpio_dt_spec PA_7 = GPIO_DT_SPEC_GET(DT_NODELABEL(ul), gpios);
+static const struct gpio_dt_spec PB_0 = GPIO_DT_SPEC_GET(DT_NODELABEL(vl), gpios);
+static const struct gpio_dt_spec PB_1 = GPIO_DT_SPEC_GET(DT_NODELABEL(wl), gpios);
+static const struct gpio_dt_spec PA_15 = GPIO_DT_SPEC_GET(DT_NODELABEL(u_in), gpios);
+static const struct gpio_dt_spec PB_3 = GPIO_DT_SPEC_GET(DT_NODELABEL(v_in), gpios);
+static const struct gpio_dt_spec PB_10 = GPIO_DT_SPEC_GET(DT_NODELABEL(w_in), gpios);
+static const struct gpio_dt_spec PB_8 = GPIO_DT_SPEC_GET(DT_NODELABEL(direction), gpios);
+static const struct gpio_dt_spec LED1 = GPIO_DT_SPEC_GET(DT_NODELABEL(direction), gpios);
+static const struct adc_dt_spec PA_0 = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+static const struct adc_dt_spec PC_1 = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+static const struct adc_dt_spec PC_0 = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+static const struct adc_dt_spec PC_2 = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+static const struct pwm_dt_spec PA_8 = PWM_DT_SPEC_GET(DT_NODELABEL(pwm_u));
+static const struct pwm_dt_spec PA_9 = PWM_DT_SPEC_GET(DT_NODELABEL(pwm_v));
+static const struct pwm_dt_spec PA_10 = PWM_DT_SPEC_GET(DT_NODELABEL(pwm_w));
---
+++ minicart_can/src/main.c
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2018 Alexander Wachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/can.h>
+
+#define SLEEP_TIME K_MSEC(250)
+
+static const struct device *const can_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
+
+static void tx_irq_callback(const struct device *dev, int error, void *arg)
+{
+	char *sender = (char *)arg;
+
+	ARG_UNUSED(dev);
+
+	if (error != 0) {
+		printf("Callback! error-code: %d\nSender: %s\n",
+		       error, sender);
+	}
+}
+
+static void send_engine_speed(const struct device *can_dev, struct can_frame *frame, int speed)
+{
+	memset(frame, 0, sizeof(struct can_frame));
+            //# Engine speed
+            //speed = self.simulator.get_engine_speed()
+            //if self.verbose:
+            //    print('engine speed = %d' % speed)
+            //if speed > 16383.75:
+            //    speed = 16383.75
+            //# Message is 1 byte unknown, 1 byte fuel level, 2 bytes engine speed (4x), fuel low @ bit 55
+
+       	//msg = [ 0, 0 ]
+	frame->data[0] = 0;
+	frame->data[1] = 0;
+        //speed *= 4
+        //msg.append(speed // 256)
+        //msg.append(speed % 256)
+	frame->data[2] = (speed * 4) / 256;
+	frame->data[3] = (speed * 4) % 256;
+        //# pad remaining bytes to make 8
+        //msg.append(0)
+        //msg.append(0)
+        //msg.append(0)
+        //msg.append(0)
+	frame->data[4] = 0;
+	frame->data[5] = 0;
+	frame->data[6] = 0;
+	frame->data[7] = 0;
+
+	frame->id = 0x3d9;
+	frame->flags = 0;
+	frame->dlc = 8;
+
+	can_send(can_dev, frame, K_FOREVER, tx_irq_callback, "send_engine_speed");
+}
+
+static void send_vehicle_speed(const struct device *can_dev, struct can_frame *frame, int speed)
+{
+	memset(frame, 0, sizeof(struct can_frame));
+        //# Vehicle speed
+        //speed = int(self.simulator.get_vehicle_speed()) % 256
+        /* Message is 15 bits speed (64x), left aligned */
+        /* Note: extra 2x to yield required left-alignment */
+	frame->data[0] = (speed * 128) / 256;
+	frame->data[1] = (speed * 128) % 256;
+        /* pad remaining bytes to make 8 */
+	frame->data[2] = 0;
+	frame->data[3] = 0;
+	frame->data[4] = 0;
+	frame->data[5] = 0;
+	frame->data[6] = 0;
+	frame->data[7] = 0;
+
+	frame->id = 0x3e9;
+	frame->flags = 0;
+	frame->dlc = 8;
+
+	can_send(can_dev, frame, K_FOREVER, tx_irq_callback, "send_vehicle_speed");
+}
+
+void send_can_data(struct can_frame *frame)
+{
+	send_engine_speed(can_dev, frame, 0);
+	send_vehicle_speed(can_dev, frame, 0);
+}
+
+int main(void)
+{
+	struct can_frame change_led_frame;
+	int ret;
+
+	if (!device_is_ready(can_dev)) {
+		printf("CAN: Device %s not ready.\n", can_dev->name);
+		return 0;
+	}
+
+#ifdef CONFIG_LOOPBACK_MODE
+	ret = can_set_mode(can_dev, CAN_MODE_LOOPBACK);
+	if (ret != 0) {
+		printf("Error setting CAN mode [%d]", ret);
+		return 0;
+	}
+#endif
+	ret = can_start(can_dev);
+	if (ret != 0) {
+		printf("Error starting CAN controller [%d]", ret);
+		return 0;
+	}
+
+	printf("Finished init.\n");
+
+	while (1) {
+		send_can_data(&change_led_frame);
+
+		k_sleep(SLEEP_TIME);
+	}
+}
