c6e6f0ab14d29a5991c0fd10b733bd1fcb22deba
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2024-02-29 07:59:59 +0900

    tmp

--- minicart/src/main.c
+++ minicart/src/main.c
@@ -1,6 +1,9 @@
-#include "mbed.h"
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/pwm.h>
 
 unsigned int q = 0, r = 0, s = 0;
 int START = 8;
@@ -12,11 +15,15 @@ static const struct gpio_dt_spec H_A = GPIO_DT_SPEC_GET(DT_NODELABEL(u_in), gpio
 static const struct gpio_dt_spec H_B = GPIO_DT_SPEC_GET(DT_NODELABEL(v_in), gpios);
 static const struct gpio_dt_spec H_C = GPIO_DT_SPEC_GET(DT_NODELABEL(w_in), gpios);
 
-static const struct adc_dt_spec V_adc = PA_0 = ADC_DT_SPEC_GET(DT_PATH(zephyr_user)); 
+static const struct adc_dt_spec V_adc = ADC_DT_SPEC_GET(DT_PATH(zephyr_user)); 
 
-PwmOut mypwmA(PA_8);  // PWM_OUT
-PwmOut mypwmB(PA_9);  // PWM_OUT
-PwmOut mypwmC(PA_10); // PWM_OUT
+static const struct pwm_dt_spec mypwm_A = PWM_DT_SPEC_GET(DT_NODELABEL(pwm_u));
+static const struct pwm_dt_spec mypwm_B = PWM_DT_SPEC_GET(DT_NODELABEL(pwm_v));
+static const struct pwm_dt_spec mypwm_C = PWM_DT_SPEC_GET(DT_NODELABEL(pwm_w));
+
+struct gpio_callback cb_H_A;
+struct gpio_callback cb_H_B;
+struct gpio_callback cb_H_C;
 
 //RawCAN can1(PA_11, PA_12);
 
@@ -28,8 +35,8 @@ unsigned int UP, VP, WP;
 
 float Vr_adc = 0.0f;
 
-Timer uT;
-float ut1 = 0, ut2 = 0, usi = 0;
+//Timer uT;
+int64_t ut0 = 0, ut1 = 0, ut2 = 0;
 float Speed = 0;
 
 char message[64];
@@ -76,50 +83,66 @@ void send_can_msg()
     dummy_speed++;
 }
 */
+
+int64_t delta_T()
+{
+	int64_t cur  = k_uptime_ticks();
+	if (ut0 > cur) {
+		return (INT64_MAX - ut0) + cur;
+	}
+
+	return cur - ut0;
+}
+
+void reset_T()
+{
+	ut0 = k_uptime_ticks();
+}
+
 void HAH() {
 
   s = r % 2;
   if (s == 0) {
-    ut1 = uT.elapsed_time().count();
+    ut1 = delta_T();
     r++;
   }
 
   if (s == 1) {
-    ut2 = uT.elapsed_time().count();
+    ut2 = delta_T();
     r++;
-    uT.reset();
+    reset_T();
   }
-  mypwmA.write(Vr_adc);
-  mypwmB.write(0);
-  mypwmC.write(0);
+  pwm_set_dt(&mypwm_A, 20000, 20000 * Vr_adc);
+  pwm_set_dt(&mypwm_B, 20000, 0);
+  pwm_set_dt(&mypwm_C, 20000, 0);
 }
 
 void HAL() {
 
-  mypwmA.write(0);
-  mypwmC.write(0);
+  pwm_set_dt(&mypwm_A, 20000, 0);
+  pwm_set_dt(&mypwm_C, 20000, 0);
 }
 void HBH() {
 
-  mypwmA.write(0);
-  mypwmB.write(Vr_adc);
-  mypwmC.write(0);
+  pwm_set_dt(&mypwm_A, 20000, 0);
+  pwm_set_dt(&mypwm_B, 20000, 20000 * Vr_adc);
+  pwm_set_dt(&mypwm_C, 20000, 0);
 }
 void HBL() {
 
-  mypwmA.write(0);
-  mypwmB.write(0);
+  pwm_set_dt(&mypwm_A, 20000, 0);
+  pwm_set_dt(&mypwm_B, 20000, 0);
 }
 void HCH() {
 
-  mypwmA.write(0);
-  mypwmB.write(0);
-  mypwmC.write(Vr_adc);
+  pwm_set_dt(&mypwm_A, 20000, 0);
+  pwm_set_dt(&mypwm_B, 20000, 0);
+  pwm_set_dt(&mypwm_C, 20000, 20000 * Vr_adc);
 }
 
 void HCL() {
-  mypwmB.write(0);
-  mypwmC.write(0);
+  pwm_set_dt(&mypwm_B, 20000, 0);
+  pwm_set_dt(&mypwm_C, 20000, 0);
 }
 
 uint32_t loop_count;
@@ -157,10 +180,6 @@ void H_C_handler(const struct device *port,
 	}
 }
 
-struct gpio_init_callback cb_H_A;
-struct gpio_init_callback cb_H_B;
-struct gpio_init_callback cb_H_C;
-
 float volume_read(const struct adc_dt_spec *dt)
 {
 	uint16_t buf;
@@ -170,7 +189,7 @@ float volume_read(const struct adc_dt_spec *dt)
 	};
 	(void)adc_sequence_init_dt(dt, &seq);
 	adc_read_dt(dt, &seq);
-	return 1.0f * buf / (2 << seq.resolution);
+	return (1.0f * buf) / BIT(seq.resolution);
 }
 
 int main() {
@@ -178,13 +197,15 @@ int main() {
 
   //can1.frequency(500000);
   //can1.mode(mbed::interface::can::Normal);
-  gpio_pin_set_dt(&EN_1, 1);
-  gpio_pin_set_dt(&EN_2, 1);
-  gpio_pin_set_dt(&EN_3, 1);
+  gpio_pin_configure_dt(&LED0, GPIO_OUTPUT);
 
-  mypwmA.period_us(20); // PWM 50KHz
-  mypwmB.period_us(20);
-  mypwmC.period_us(20);
+  gpio_pin_configure_dt(&EN_1, GPIO_OUTPUT);
+  gpio_pin_configure_dt(&EN_2, GPIO_OUTPUT);
+  gpio_pin_configure_dt(&EN_3, GPIO_OUTPUT);
+
+  gpio_pin_configure_dt(&H_A, GPIO_INPUT);
+  gpio_pin_configure_dt(&H_B, GPIO_INPUT);
+  gpio_pin_configure_dt(&H_C, GPIO_INPUT);
 
   gpio_pin_interrupt_configure_dt(&H_A, GPIO_INT_EDGE_BOTH);
   gpio_pin_interrupt_configure_dt(&H_B, GPIO_INT_EDGE_BOTH);
@@ -194,16 +215,25 @@ int main() {
   gpio_init_callback(&cb_H_B, H_B_handler, BIT(H_B.pin));
   gpio_init_callback(&cb_H_C, H_B_handler, BIT(H_C.pin));
 
-  gpio_add_callback_dt(&H_A, cb_H_A);
-  gpio_add_callback_dt(&H_B, cb_H_B);
-  gpio_add_callback_dt(&H_C, cb_H_C);
+  //gpio_add_callback_dt(&H_A, &cb_H_A);
+  //gpio_add_callback_dt(&H_B, &cb_H_B);
+  //gpio_add_callback_dt(&H_C, &cb_H_C);
 
+  adc_channel_setup_dt(&V_adc);
 
+  gpio_pin_set_dt(&EN_1, 1);
+  gpio_pin_set_dt(&EN_2, 1);
+  gpio_pin_set_dt(&EN_3, 1);
+
+  reset_T();
+
+	pwm_set_dt(&mypwm_A, 20000000, 10000000);
+	pwm_set_dt(&mypwm_B, 20000000, 10000000);
+	pwm_set_dt(&mypwm_C, 20000000, 10000000);
 
   while (1) {
-
+#if 0
     Vr_adc = volume_read(&V_adc);
-    uT.start();
 
     if (loop_count == 1) {
       //send_can_msg();
@@ -215,20 +245,20 @@ int main() {
     if ((Vr_adc > 0.15f) && (q == 0)) {
       while (q < 50) {
 
-        mypwmA.write(0);
-        mypwmB.write(0.5f);
-        mypwmC.write(0);
-	k_usleep(START);
+	pwm_set_dt(&mypwm_A, 20000, 0);
+	pwm_set_dt(&mypwm_B, 20000, 10000);
+	pwm_set_dt(&mypwm_C, 20000, 0);
+	k_msleep(START);
 
-        mypwmA.write(0.5f);
-        mypwmB.write(0);
-        mypwmC.write(0);
-	k_usleep(START);
+	pwm_set_dt(&mypwm_A, 20000, 10000);
+	pwm_set_dt(&mypwm_B, 20000, 0);
+	pwm_set_dt(&mypwm_C, 20000, 0);
+	k_msleep(START);
 
-        mypwmA.write(0);
-        mypwmB.write(0);
-        mypwmC.write(0.5f);
-	k_usleep(START);
+	pwm_set_dt(&mypwm_A, 20000, 0);
+	pwm_set_dt(&mypwm_B, 20000, 0);
+	pwm_set_dt(&mypwm_C, 20000, 10000);
+	k_msleep(START);
         q++;
       }
     }
@@ -238,14 +268,14 @@ int main() {
       q = 0;
     }
 
-    usi = fabs(ut2 - ut1);
-    Speed = 60 * (1.f / (7.0f * usi * float(1E-6)));
-    snprintf(message, sizeof(message), "%d.%03d , %d.%03d \r", (int)Speed, int(Speed * 1000.0f) % 1000, 
-                                                             (int)Vr_adc, int(Vr_adc * 1000.0f) % 1000);
+    Speed = 60 * (1.f / (7.0f * k_cyc_to_us_floor64(abs(ut2 - ut1)) * 1E-6f));
+    printf("%d.%03d , %d.%03d \r", (int)Speed, (int)(Speed * 1000.0f) % 1000, 
+                                                             (int)Vr_adc, (int)(Vr_adc * 1000.0f) % 1000);
     //pc.write(message, sizeof(message));
     // UP=HA; VP=HB; WP=HC;
     // pc.printf("%d  ,%d ,%d\r" ,UP,VP,WP);
     //myled = !myled;
+#endif
     gpio_pin_toggle_dt(&LED0);
   }
 }
