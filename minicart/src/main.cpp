fe95a7654ec87262528e59b27ae8e155ce913230
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2024-03-13 18:02:09 +0900

    x

--- minicart/src/main.cpp
+++ minicart/src/main.cpp
@@ -10,6 +10,9 @@
 /*******************20210526*******************************/
 #include "mbed.h"
 #include <math.h>
+#include <zephyr/drivers/can.h>
+
+static const struct device *const can_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
 
 #define VMAX_SET  1.0/*PI制御最大電圧指令0.0～1.0*/
 #define IMAX_SET  0.4/* PI制御最大電流指令0.0～1.0*/
@@ -138,6 +141,93 @@ Ticker Cu(DEVICE_DT_GET(DT_NODELABEL(counter17)));
 
 /*******************************************/
  float  PWMDuty=0;
+
+
+
+static void tx_irq_callback(const struct device *dev, int error, void *arg)
+{
+	char *sender = (char *)arg;
+
+	ARG_UNUSED(dev);
+
+	if (error != 0) {
+		printf("Callback! error-code: %d\nSender: %s\n",
+		       error, sender);
+	}
+}
+
+int dummy_speed;
+
+static void send_engine_speed(const struct device *can_dev, struct can_frame *frame, int speed)
+{
+	//speed = dummy_speed;
+	dummy_speed++;
+
+	memset(frame, 0, sizeof(struct can_frame));
+            //# Engine speed
+            //speed = self.simulator.get_engine_speed()
+            //if self.verbose:
+            //    print('engine speed = %d' % speed)
+            //if speed > 16383.75:
+            //    speed = 16383.75
+            //# Message is 1 byte unknown, 1 byte fuel level, 2 bytes engine speed (4x), fuel low @ bit 55
+
+       	//msg = [ 0, 0 ]
+	frame->data[0] = 0;
+	frame->data[1] = 0;
+        //speed *= 4
+        //msg.append(speed // 256)
+        //msg.append(speed % 256)
+	frame->data[2] = (speed * 4) / 256;
+	frame->data[3] = (speed * 4) % 256;
+        //# pad remaining bytes to make 8
+        //msg.append(0)
+        //msg.append(0)
+        //msg.append(0)
+        //msg.append(0)
+	frame->data[4] = 0;
+	frame->data[5] = 0;
+	frame->data[6] = 0;
+	frame->data[7] = 0;
+
+	frame->id = 0x3d9;
+	frame->flags = 0;
+	frame->dlc = 8;
+
+	can_send(can_dev, frame, K_FOREVER, tx_irq_callback, const_cast<char*>("send_engine_speed"));
+}
+
+static void send_vehicle_speed(const struct device *can_dev, struct can_frame *frame, int speed)
+{
+	memset(frame, 0, sizeof(struct can_frame));
+        //# Vehicle speed
+        //speed = int(self.simulator.get_vehicle_speed()) % 256
+        /* Message is 15 bits speed (64x), left aligned */
+        /* Note: extra 2x to yield required left-alignment */
+	frame->data[0] = ((speed/60) * 128) / 256;
+	frame->data[1] = ((speed/60) * 128) % 256;
+        /* pad remaining bytes to make 8 */
+	frame->data[2] = 0;
+	frame->data[3] = 0;
+	frame->data[4] = 0;
+	frame->data[5] = 0;
+	frame->data[6] = 0;
+	frame->data[7] = 0;
+
+	frame->id = 0x3e9;
+	frame->flags = 0;
+	frame->dlc = 8;
+
+	can_send(can_dev, frame, K_FOREVER, tx_irq_callback, const_cast<char*>("send_vehicle_speed"));
+}
+
+void send_can_data(struct can_frame *frame)
+{
+	send_engine_speed(can_dev, frame, 0);
+	//send_vehicle_speed(can_dev, frame, 0);
+}
+
+
 /**************Hall Sensor******************/
 unsigned char UVW_in(void)
 {
@@ -409,6 +499,8 @@ int current_pi_count;
     
 /********main***********/
 int main() {
+  struct can_frame change_led_frame;
+  int ret;
     //Timer1.start();  
     uTimer.start();
     vTimer.start();
@@ -424,6 +516,27 @@ int main() {
 
     wait_ms(500);    
     Vr_adc_i=V_adc.read();  
+
+
+	if (!device_is_ready(can_dev)) {
+		printf("CAN: Device %s not ready.\n", can_dev->name);
+		return 0;
+	}
+
+#ifdef CONFIG_LOOPBACK_MODE
+	ret = can_set_mode(can_dev, CAN_MODE_LOOPBACK);
+	if (ret != 0) {
+		printf("Error setting CAN mode [%d]", ret);
+		return 0;
+	}
+#endif
+	ret = can_start(can_dev);
+	if (ret != 0) {
+		printf("Error starting CAN controller [%d]", ret);
+		return 0;
+	}
+
+
 
      
 #ifdef TickSpeed
@@ -450,6 +563,7 @@ int main() {
 
 #else
  while(1) {
+   send_can_data(&change_led_frame);
    //wait_us(50); //20
    //pc.printf("%.3f,%.3f,%.3f \r" ,du,dv,dw);
    vr_ad=V_adc.read();
