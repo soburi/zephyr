8dd104115468ac44972cffe45f1ebec54debc0d5
Andrew Featherstone <andrew.featherstone@gmail.com>
2024-09-13 13:56:22 +0100

    soc: rp2350: Handle API change for deprecated z_arm_platform_init
    https://github.com/zephyrproject-rtos/zephyr/pull/62595 introduced
    changes to the kernel API. Update soc/raspberrypi/rpi_pico/rp2350/soc.c
    to handle this change. If the PR isn't going to be reverted, then this
    commit should be squashed into 50526fc6182 , but defer that until
    https://github.com/zephyrproject-rtos/zephyr/issues/78386 has been
    commented on, and leave this as a distinct commit that might be
    reverted.
    
    Signed-off-by: Andrew Featherstone <andrew.featherstone@gmail.com>

--- soc/raspberrypi/rpi_pico/rp2350/soc.c
+++ soc/raspberrypi/rpi_pico/rp2350/soc.c
@@ -12,13 +12,13 @@
  * for the Raspberry Pi RP235xx (RP2350A, RP2350B, RP2354A, RP2354B).
  */
 
-#if CONFIG_PLATFORM_SPECIFIC_INIT
+#if CONFIG_SOC_RESET_HOOK
 #include <pico/runtime_init.h>
 
 
-void z_arm_platform_init(void)
+void soc_reset_hook(void)
 {
 	runtime_init_per_core_enable_coprocessors();
 }
 
-#endif /* CONFIG_PLATFORM_SPECIFIC_INIT */
+#endif /* CONFIG_SOC_RESET_HOOK */
