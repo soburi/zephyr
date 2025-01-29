387c9ff3d0403bb1deb4a8a5055700602361ffdd
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-01-30 06:37:03 +0900

    x

--- boards/raspberrypi/rpi_pico2/board.cmake
+++ boards/raspberrypi/rpi_pico2/board.cmake
@@ -16,3 +16,4 @@ board_runner_args(uf2 "--board-id=RP2350")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
