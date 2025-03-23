c8ee93bb2a3a4cbd5aab3f608a0b30eb26342913
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-03-23 20:07:55 +0900

    scripts: dts: Add handling for `*-map` property
    This change introduces generating definitions corresponding to
    `*-map` property, which was currently discarded.
    
    For `*-map` properties are made able to be treated as a variation
    of phandle-array, assign sequential cell names for each group of
    specifiers (child_specifier_0, child_specifier_1, ...,
    parent_specifier_0, ...).
    
    The `*-map` data is like a two-dimensional array, so it is difficult to
    handle with the existing APIs, so we will also provide new APIs.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- scripts/dts/python-devicetree/src/devicetree/edtlib.py
+++ scripts/dts/python-devicetree/src/devicetree/edtlib.py
@@ -1046,6 +1046,14 @@ class Node:
       list is empty if the node does not hog any GPIOs. Only relevant for GPIO hog
       nodes.
 
+    maps:
+      A list of ControllerAndData objects for each entry in a *-map property on
+      the node, such as 'interrupt-map'. Each ControllerAndData describes a
+      mapping from a child specifier to a parent controller, with 'basename' set
+      to the specifier name (e.g. 'interrupt') and 'data' containing the
+      parsed child and parent specifier cells.
+      The list is empty if the node has no *-map properties.
+
     is_pci_device:
       True if the node is a PCI device.
     """
@@ -1318,6 +1326,96 @@ class Node:
                 node=self, controller=controller,
                 data=self._named_cells(controller, item, "gpio"),
                 name=None, basename="gpio"))
+
+        return res
+
+    @property
+    def maps(self) -> list[ControllerAndData]:
+        "See the class docstring"
+
+        res: list[ControllerAndData] = []
+
+        def count_cells_num(node: dtlib_Node, specifier: str) -> int:
+            """
+            Calculate the number of cells in the node.
+            When calculating the number of interrupt cells,
+            add up the values of the address cells.
+            """
+
+            num = node.props[f"#{specifier}-cells"].to_num()
+
+            if specifier == "interrupt":
+                parent_props = None
+                if node.parent:
+                    parent_props = node.parent.props
+
+                if "#address-cells" in node.props:
+                    num = num + node.props["#address-cells"].to_num()
+                elif parent_props and "#address-cells" in parent_props:
+                    num = num + parent_props["#address-cells"].to_num()
+                else:
+                    _err("Neither the node nor its parent has `#address-cells` property")
+
+            return num
+
+        for prop in [v for k, v in self._node.props.items() if k.endswith("-map")]:
+            specifier_space = prop.name[:-4]  # Strip '-map'
+            raw = prop.value
+            while raw:
+                if len(raw) < 4:
+                    # Not enough room for phandle
+                    _err("bad value for " + repr(prop))
+
+                child_specifier_num = count_cells_num(prop.node, specifier_space)
+
+                child_specifiers = to_nums(raw[: 4 * child_specifier_num])
+                raw = raw[4 * child_specifier_num :]
+                phandle = to_num(raw[:4])
+                raw = raw[4:]
+
+                controller_node = prop.node.dt.phandle2node.get(phandle)
+                if controller_node is None:
+                    _err(f"controller node cannot be found from phandle:{phandle}")
+
+                controller: Node = self.edt._node2enode[controller_node]
+                if controller is None:
+                    _err("controller cannot be found from: " + repr(controller_node))
+
+                parent_specifier_num = count_cells_num(controller_node, specifier_space)
+                parent_specifiers = to_nums(raw[: 4 * parent_specifier_num])
+                raw = raw[4 * parent_specifier_num :]
+
+                # Although this is rare, if a cell-name is specified for the map node,
+                # it will be reflected.
+                # If not specified, the name of child_specifier_[i] will be set.
+                values: dict[str, int] = {}
+                for i, v in enumerate(child_specifiers):
+                    cell_name = f"child_specifier_{i}"
+                    if (self._binding and
+                        self._binding.specifier2cells and
+                        specifier_space in self._binding.specifier2cells and
+                        i < len(self._binding.specifier2cells[specifier_space])):
+                        cell_name = self._binding.specifier2cells[specifier_space][i]
+
+                    values[cell_name] = v
+
+                # The cell name for parent_specifier cannot be determined.
+                # For convenience, we assign it the name parent_specifier_[i].
+                for i, v in enumerate(parent_specifiers):
+                    values[f"parent_specifier_{i}"] = v
+
+                res.append(
+                    ControllerAndData(
+                        node=self,
+                        controller=controller,
+                        data=values,
+                        name=None,
+                        basename=specifier_space,
+                    )
+                )
+
+            if len(raw) != 0:
+                _err(f"unexpected prop.value remainings: {raw}")
 
         return res
 
