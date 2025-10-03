5e7a11bd8ad93aece5c53f3d1a10760d56037665
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-10-04 00:04:34 +0900

    x

--- samples/drivers/audio/aw88298/src/main.c
+++ samples/drivers/audio/aw88298/src/main.c
@@ -3,187 +3,288 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#define CONFIG_USE_CODEC_CLOCK 1
 
-#include <stdint.h>
-
-#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/audio/dmic.h>
 #include <zephyr/drivers/i2s.h>
 #include <zephyr/audio/codec.h>
-#include <zephyr/kernel.h>
-#include <zephyr/sys/util.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/toolchain.h>
+#include <string.h>
 
-#define SAMPLE_FREQUENCY 48000U
-#define SAMPLE_WORD_SIZE 16U
-#define CHANNEL_COUNT    2U
+#ifndef CONFIG_USE_DMIC
+#include "sine.h"
+#endif
 
 static const int16_t sine_wave[] = {
  3211,   6392,   9511,  12539,  15446,  18204,  20787,  23169,
 25329,  27244,  28897,  30272,  31356,  32137,  32609,  32767,
 32609,  32137,  31356,  30272,  28897,  27244,  25329,  23169,
-20787,  18204,  15446,  12539,   9511,   6392,   3211,      0,
+20787,  18204,  15446,  12539,   9511,   6392,   3211,	  0,
 -3212,  -6393,  -9512, -12540, -15447, -18205, -20788, -23170,
 -25330, -27245, -28898, -30273, -31357, -32138, -32610, -32767,
 -32610, -32138, -31357, -30273, -28898, -27245, -25330, -23170,
--20788, -18205, -15447, -12540,  -9512,  -6393,  -3212,     -1,
+-20788, -18205, -15447, -12540,  -9512,  -6393,  -3212,	 -1,
 };
 
-#define SAMPLES_PER_BLOCK ARRAY_SIZE(sine_wave)
-#define BLOCK_SIZE        (SAMPLES_PER_BLOCK * CHANNEL_COUNT * sizeof(int16_t))
-#define INITIAL_BLOCKS    4U
-#define BLOCK_COUNT       (INITIAL_BLOCKS + 4U)
-#define PLAYBACK_SECONDS  4U
-#define TOTAL_BLOCKS      ((PLAYBACK_SECONDS * SAMPLE_FREQUENCY) / SAMPLES_PER_BLOCK)
+#define I2S_CODEC_TX DT_ALIAS(i2s_codec_tx)
 
-K_MEM_SLAB_DEFINE(tx_mem_slab, BLOCK_SIZE, BLOCK_COUNT, 4);
+#define SAMPLE_FREQUENCY 48000U
+#define SAMPLE_BIT_WIDTH (16U)
+#define BYTES_PER_SAMPLE sizeof(int16_t)
+#if CONFIG_USE_DMIC
+#define NUMBER_OF_CHANNELS CONFIG_DMIC_CHANNELS
+#else
+#define NUMBER_OF_CHANNELS (2U)
+#endif
+/* Such block length provides an echo with the delay of 100 ms. */
+#define SAMPLES_PER_BLOCK ARRAY_SIZE(sine_wave)
+#define INITIAL_BLOCKS	4 //CONFIG_I2S_INIT_BUFFERS
+#define TIMEOUT           (2000U)
+
+#define BLOCK_SIZE  (SAMPLES_PER_BLOCK * NUMBER_OF_CHANNELS * sizeof(int16_t))
+#define BLOCK_COUNT (INITIAL_BLOCKS + 4U)
+
+K_MEM_SLAB_DEFINE(mem_slab, BLOCK_SIZE, BLOCK_COUNT, 4);
+
+
+#define PLAYBACK_SECONDS  4U
+#define TOTAL_BLOCKS	  ((PLAYBACK_SECONDS * SAMPLE_FREQUENCY) / SAMPLES_PER_BLOCK)
+
 
 static void fill_block(int16_t *buffer, uint32_t block_idx)
 {
-    const size_t count = ARRAY_SIZE(sine_wave);
-    const uint32_t attenuation = block_idx % 3U;
+	const size_t count = ARRAY_SIZE(sine_wave);
+	const uint32_t attenuation = block_idx % 3U;
 
-    for (size_t i = 0; i < count; i++) {
-        int16_t sample_l = sine_wave[i] >> attenuation;
-        size_t right_index = (i + count / 4U) % count;
-        int16_t sample_r = sine_wave[right_index] >> attenuation;
+	for (size_t i = 0; i < count; i++) {
+		int16_t sample_l = sine_wave[i] >> attenuation;
+		size_t right_index = (i + count / 4U) % count;
+		int16_t sample_r = sine_wave[right_index] >> attenuation;
 
-        buffer[CHANNEL_COUNT * i] = sample_l;
-        buffer[CHANNEL_COUNT * i + 1] = sample_r;
-    }
+		buffer[NUMBER_OF_CHANNELS * i] = sample_l;
+		buffer[NUMBER_OF_CHANNELS * i + 1] = sample_r;
+	}
+}
+
+static bool configure_tx_streams(const struct device *i2s_dev, struct i2s_config *config)
+{
+	int ret;
+
+	ret = i2s_configure(i2s_dev, I2S_DIR_TX, config);
+	if (ret < 0) {
+		printk("Failed to configure codec stream: %d\n", ret);
+		return false;
+	}
+
+	return true;
+}
+
+static bool trigger_command(const struct device *i2s_dev_codec, enum i2s_trigger_cmd cmd)
+{
+	int ret;
+
+	ret = i2s_trigger(i2s_dev_codec, I2S_DIR_TX, cmd);
+	if (ret < 0) {
+		printk("Failed to trigger command %d on TX: %d\n", cmd, ret);
+		return false;
+	}
+
+	return true;
 }
 
 int main(void)
 {
-    const struct device *const i2s_dev = DEVICE_DT_GET(DT_ALIAS(i2s_tx));
-    const struct device *const codec_dev = DEVICE_DT_GET(DT_ALIAS(i2s_codec_tx));
-    struct i2s_config i2s_cfg = {0};
-    struct audio_codec_cfg codec_cfg = {0};
-    audio_property_value_t volume = {.vol = 80};
-    bool codec_started = false;
-    bool i2s_started = false;
-    int ret = 0;
+	const struct device *const i2s_dev_codec = DEVICE_DT_GET(DT_ALIAS(i2s_tx));
+#if CONFIG_USE_DMIC
+	const struct device *const dmic_dev = DEVICE_DT_GET(DT_NODELABEL(dmic_dev));
+#endif
+	const struct device *const codec_dev = DEVICE_DT_GET(DT_ALIAS(i2s_codec_tx));
+	struct i2s_config config;
+	struct audio_codec_cfg audio_cfg;
+	audio_property_value_t volume = {.vol = 80};
+	bool codec_started = false;
+	bool i2s_started = false;
+	int ret = 0;
 
-    printk("AW88298 playback sample\n");
+#if CONFIG_USE_DMIC
+	struct pcm_stream_cfg stream = {
+		.pcm_width = SAMPLE_BIT_WIDTH,
+		.mem_slab = &mem_slab,
+	};
+	struct dmic_cfg cfg = {
+		.io = {
+			/* These fields can be used to limit the PDM clock
+			 * configurations that the driver is allowed to use
+			 * to those supported by the microphone.
+			 */
+			.min_pdm_clk_freq = 1000000,
+			.max_pdm_clk_freq = 3500000,
+			.min_pdm_clk_dc   = 40,
+			.max_pdm_clk_dc   = 60,
+		},
+		.streams = &stream,
+		.channel = {
+			.req_num_streams = 1,
+		},
+	};
+#endif
+	printk("codec sample\n");
 
-    if (!device_is_ready(i2s_dev)) {
-        printk("I2S device %s is not ready\n", i2s_dev->name);
-        return -ENODEV;
-    }
+#if CONFIG_USE_DMIC
+	if (!device_is_ready(dmic_dev)) {
+		printk("%s is not ready", dmic_dev->name);
+		return 0;
+	}
+#endif
 
-    if (!device_is_ready(codec_dev)) {
-        printk("Codec device %s is not ready\n", codec_dev->name);
-        return -ENODEV;
-    }
+	if (!device_is_ready(i2s_dev_codec)) {
+		printk("%s is not ready\n", i2s_dev_codec->name);
+		return 0;
+	}
 
-    i2s_cfg.word_size = SAMPLE_WORD_SIZE;
-    i2s_cfg.channels = CHANNEL_COUNT;
-    i2s_cfg.format = I2S_FMT_DATA_FORMAT_I2S;
-    i2s_cfg.options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
-    i2s_cfg.frame_clk_freq = SAMPLE_FREQUENCY;
-    i2s_cfg.mem_slab = &tx_mem_slab;
-    i2s_cfg.block_size = BLOCK_SIZE;
-    i2s_cfg.timeout = 2000U;
+	if (!device_is_ready(codec_dev)) {
+		printk("%s is not ready", codec_dev->name);
+		return 0;
+	}
+	audio_cfg.dai_route = AUDIO_ROUTE_PLAYBACK;
+	audio_cfg.dai_type = AUDIO_DAI_TYPE_I2S;
+	audio_cfg.dai_cfg.i2s.word_size = SAMPLE_BIT_WIDTH;
+	audio_cfg.dai_cfg.i2s.channels = 2;
+	audio_cfg.dai_cfg.i2s.format = I2S_FMT_DATA_FORMAT_I2S;
+	audio_cfg.dai_cfg.i2s.options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
+	audio_cfg.dai_cfg.i2s.frame_clk_freq = SAMPLE_FREQUENCY;
+	audio_codec_configure(codec_dev, &audio_cfg);
+	k_msleep(1000);
 
-    ret = i2s_configure(i2s_dev, I2S_DIR_TX, &i2s_cfg);
-    if (ret < 0) {
-        printk("Failed to configure I2S: %d\n", ret);
-        return ret;
-    }
+#if CONFIG_USE_DMIC
+	cfg.channel.req_num_chan = 2;
+	cfg.channel.req_chan_map_lo = dmic_build_channel_map(0, 0, PDM_CHAN_LEFT) |
+				      dmic_build_channel_map(1, 1, PDM_CHAN_RIGHT);
+	cfg.streams[0].pcm_rate = SAMPLE_FREQUENCY;
+	cfg.streams[0].block_size = BLOCK_SIZE;
 
-    codec_cfg.dai_type = AUDIO_DAI_TYPE_I2S;
-    codec_cfg.dai_route = AUDIO_ROUTE_PLAYBACK;
-    codec_cfg.dai_cfg.i2s.word_size = SAMPLE_WORD_SIZE;
-    codec_cfg.dai_cfg.i2s.channels = CHANNEL_COUNT;
-    codec_cfg.dai_cfg.i2s.format = I2S_FMT_DATA_FORMAT_I2S;
-    codec_cfg.dai_cfg.i2s.options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
-    codec_cfg.dai_cfg.i2s.frame_clk_freq = SAMPLE_FREQUENCY;
+	printk("PCM output rate: %u, channels: %u\n", cfg.streams[0].pcm_rate,
+	       cfg.channel.req_num_chan);
 
-    ret = audio_codec_configure(codec_dev, &codec_cfg);
-    if (ret < 0) {
-        printk("Failed to configure codec: %d\n", ret);
-        return ret;
-    }
+	ret = dmic_configure(dmic_dev, &cfg);
+	if (ret < 0) {
+		printk("Failed to configure the driver: %d", ret);
+		return ret;
+	}
+#endif
 
-    ret = audio_codec_set_property(codec_dev, AUDIO_PROPERTY_OUTPUT_VOLUME,
-                                   AUDIO_CHANNEL_ALL, volume);
-    if (ret < 0) {
-        printk("Failed to set codec volume: %d\n", ret);
-    }
+	config.word_size = SAMPLE_BIT_WIDTH;
+	config.channels = NUMBER_OF_CHANNELS;
+	config.format = I2S_FMT_DATA_FORMAT_I2S;
+	config.options = I2S_OPT_BIT_CLK_MASTER | I2S_OPT_FRAME_CLK_MASTER;
+	config.frame_clk_freq = SAMPLE_FREQUENCY;
+	config.mem_slab = &mem_slab;
+	config.block_size = BLOCK_SIZE;
+	config.timeout = TIMEOUT;
+	if (!configure_tx_streams(i2s_dev_codec, &config)) {
+		printk("failure to config streams\n");
+		return 0;
+	}
 
-    ret = audio_codec_apply_properties(codec_dev);
-    if (ret < 0) {
-        printk("Failed to apply codec properties: %d\n", ret);
-    }
 
-    for (uint32_t i = 0U; i < INITIAL_BLOCKS; i++) {
-        void *block;
+	ret = audio_codec_set_property(codec_dev, AUDIO_PROPERTY_OUTPUT_VOLUME,
+								   AUDIO_CHANNEL_ALL, volume);
+	if (ret < 0) {
+		printk("Failed to set codec volume: %d\n", ret);
+	}
 
-        ret = k_mem_slab_alloc(&tx_mem_slab, &block, K_FOREVER);
-        if (ret < 0) {
-            printk("Failed to allocate TX block: %d\n", ret);
-            return ret;
-        }
+	ret = audio_codec_apply_properties(codec_dev);
+	if (ret < 0) {
+		printk("Failed to apply codec properties: %d\n", ret);
+	}
 
-        fill_block((int16_t *)block, i);
+	printk("start streams\n");
+	for (;;) {
+		bool started = false;
+#if CONFIG_USE_DMIC
+		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_START);
+		if (ret < 0) {
+			printk("START trigger failed: %d", ret);
+			return ret;
+		}
+#endif
+		for (uint32_t i = 0U; i < INITIAL_BLOCKS; i++) {
+			void *mem_block;
 
-        ret = i2s_write(i2s_dev, block, BLOCK_SIZE);
-        if (ret < 0) {
-            printk("Failed to queue TX block: %d\n", ret);
-            return ret;
-        }
-    }
+			ret = k_mem_slab_alloc(&mem_slab, &mem_block, K_FOREVER);
+			if (ret < 0) {
+				printk("Failed to allocate TX mem_block: %d\n", ret);
+				return ret;
+			}
 
-    audio_codec_start_output(codec_dev);
-    codec_started = true;
+			fill_block((int16_t *)mem_block, i);
 
-    ret = i2s_trigger(i2s_dev, I2S_DIR_TX, I2S_TRIGGER_START);
-    if (ret < 0) {
-        printk("Failed to start I2S stream: %d\n", ret);
-        goto cleanup;
-    }
-    i2s_started = true;
+			ret = i2s_write(i2s_dev_codec, mem_block, BLOCK_SIZE);
+			if (ret < 0) {
+				printk("Failed to queue TX mem_block: %d\n", ret);
+				return ret;
+			}
+		}
 
-    for (uint32_t block_idx = INITIAL_BLOCKS; block_idx < TOTAL_BLOCKS; block_idx++) {
-        void *block;
+		audio_codec_start_output(codec_dev);
+		codec_started = true;
 
-        ret = k_mem_slab_alloc(&tx_mem_slab, &block, K_FOREVER);
-        if (ret < 0) {
-            printk("Failed to allocate TX block: %d\n", ret);
-            goto cleanup;
-        }
+		ret = i2s_trigger(i2s_dev_codec, I2S_DIR_TX, I2S_TRIGGER_START);
+		if (ret < 0) {
+			printk("Failed to start I2S stream: %d\n", ret);
+			goto cleanup;
+		}
+		i2s_started = true;
 
-        fill_block((int16_t *)block, block_idx);
+		for (uint32_t block_idx = INITIAL_BLOCKS; block_idx < TOTAL_BLOCKS; block_idx++) {
+			void *mem_block;
 
-        ret = i2s_write(i2s_dev, block, BLOCK_SIZE);
-        if (ret < 0) {
-            printk("Failed to queue TX block: %d\n", ret);
-            goto cleanup;
-        }
-    }
+			ret = k_mem_slab_alloc(&mem_slab, &mem_block, K_FOREVER);
+			if (ret < 0) {
+				printk("Failed to allocate TX mem_block: %d\n", ret);
+				goto cleanup;
+			}
 
-    ret = i2s_trigger(i2s_dev, I2S_DIR_TX, I2S_TRIGGER_DRAIN);
-    if (ret < 0) {
-        printk("Failed to drain I2S stream: %d\n", ret);
-        goto cleanup;
-    }
+			fill_block((int16_t *)mem_block, block_idx);
 
-    i2s_started = false;
+			ret = i2s_write(i2s_dev_codec, mem_block, BLOCK_SIZE);
+			if (ret < 0) {
+				printk("Failed to queue TX mem_block: %d\n", ret);
+				goto cleanup;
+			}
+		}
+
+		ret = i2s_trigger(i2s_dev_codec, I2S_DIR_TX, I2S_TRIGGER_DRAIN);
+		if (ret < 0) {
+			printk("Failed to drain I2S stream: %d\n", ret);
+			goto cleanup;
+		}
+
+		i2s_started = false;
 
 cleanup:
-    if (i2s_started) {
-        (void)i2s_trigger(i2s_dev, I2S_DIR_TX, I2S_TRIGGER_STOP);
-    }
+		if (i2s_started) {
+			(void)i2s_trigger(i2s_dev_codec, I2S_DIR_TX, I2S_TRIGGER_STOP);
+		}
 
-    if (codec_started) {
-        audio_codec_stop_output(codec_dev);
-    }
+		if (codec_started) {
+			audio_codec_stop_output(codec_dev);
+		}
 
-    if (ret < 0) {
-        printk("Playback finished with error: %d\n", ret);
-        return ret;
-    }
-
-    printk("AW88298 playback complete\n");
-
-    return 0;
+		if (ret < 0) {
+			printk("Playback finished with error: %d\n", ret);
+			return ret;
+		}
+#if CONFIG_USE_DMIC
+		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
+		if (ret < 0) {
+			printk("STOP trigger failed: %d", ret);
+			return 0;
+		}
+#endif
+		printk("Streams stopped\n");
+		return 0;
+	}
 }
