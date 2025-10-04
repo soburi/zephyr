b81de79d9b1774dd3d9c7cde074bd7be61b27e09
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-10-04 23:29:44 +0900

    critical

--- soc/renesas/ra/ra4m1/soc.h
+++ soc/renesas/ra/ra4m1/soc.h
@@ -11,6 +11,12 @@
 #ifndef ZEPHYR_SOC_RENESAS_RA4M1_SOC_H_
 #define ZEPHYR_SOC_RENESAS_RA4M1_SOC_H_
 
+#include <zephyr/irq.h>
+
+#define FSP_CRITICAL_SECTION_DEFINE            unsigned int irq_lock_key
+#define FSP_CRITICAL_SECTION_ENTER             irq_lock_key = irq_lock();
+#define FSP_CRITICAL_SECTION_EXIT              irq_unlock(irq_lock_key);
+
 #include <bsp_api.h>
 
 #endif /* ZEPHYR_SOC_RENESAS_RA4M1_SOC_H_ */
