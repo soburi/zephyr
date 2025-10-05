81aa00aecda37efbd60d97308fec0687107f8c56
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-10-06 08:13:40 +0900

    x

--- soc/raspberrypi/rpi_pico/common/binary_info.c
+++ soc/raspberrypi/rpi_pico/common/binary_info.c
@@ -70,7 +70,7 @@
 /* Encode the pins in a group */
 
 #define IS_LAST_PIN(end, idx, off) (((idx) + (off) + 1) == (end))
-#define PIN_ENTRY(n, p, i, off, end) \
+#define PIN_ENTRY(n, p, i, off, end)                                                               \
 	(DT_PROP_HAS_IDX(n, p, i) ? ENCODE_PIN(n, i, off) + IS_LAST_PIN(end, i, off) : 0)
 #define ENCODE_EACH_PIN(n, p, i, end)                                                              \
 	PIN_ENTRY(n, p, i, PIN_GROUP_OFFSET(DT_PARENT(n), DT_NODE_CHILD_IDX(n)), end)
