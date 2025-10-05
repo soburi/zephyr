874da490597b4910e76e4629f7a930439a3f24a5
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-10-06 08:13:33 +0900

    x

--- soc/raspberrypi/rpi_pico/common/binary_info.c
+++ soc/raspberrypi/rpi_pico/common/binary_info.c
@@ -70,12 +70,10 @@
 /* Encode the pins in a group */
 
 #define IS_LAST_PIN(end, idx, off) (((idx) + (off) + 1) == (end))
-#define PIN_TERMINATE(n, p, i, off, end)                                                           \
-	(IS_LAST_PIN(end, i, off) ? ENCODE_PIN(n, i, (off) + 1) : ENCODE_PIN(n, i, off))
-#define PIN_ENTRY(n, p, i, off, end) (DT_PROP_HAS_IDX(n, p, i) ? PIN_TERMINATE(n, p, i, off, end) : 0)
+#define PIN_ENTRY(n, p, i, off, end) \
+	(DT_PROP_HAS_IDX(n, p, i) ? ENCODE_PIN(n, i, off) + IS_LAST_PIN(end, i, off) : 0)
 #define ENCODE_EACH_PIN(n, p, i, end)                                                              \
-	PIN_ENTRY(n, p, i, PIN_GROUP_OFFSET(DT_PARENT(n), DT_NODE_CHILD_IDX(n)), end) |                 \
-		0
+	PIN_ENTRY(n, p, i, PIN_GROUP_OFFSET(DT_PARENT(n), DT_NODE_CHILD_IDX(n)), end)
 #define ENCODE_GROUP_PINS(n) (FOREACH_PIN_GROUP(n, (|), ENCODE_EACH_PIN, PIN_GROUP_AMOUNT(n)))
 
 /* Get group-wide pin functions */
