06da95ca12a9782aa74a2da1dc8828768f903ca3
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-01-02 00:07:38 +0900

    x

--- drivers/gpio/gpio_rp1.c
+++ drivers/gpio/gpio_rp1.c
@@ -12,8 +12,8 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_utils.h>
 
-#define GPIO_STATUS(base, n) (base + 0x8 * n)
-#define GPIO_CTRL(base, n)   (GPIO_STATUS(base, n) + 0x4)
+#define GPIO_STATUS(port, n) (DEVICE_MMIO_NAMED_GET(port, gpio) + 0x8 * n)
+#define GPIO_CTRL(port, n)   (GPIO_STATUS(port, n) + 0x4)
 
 #define GPIO_STATUS_OUT_TO_PAD    0x200
 #define GPIO_STATUS_OUT_FROM_PERI 0x100
@@ -27,20 +27,20 @@
 #define GPIO_CTRL_FUNCSEL_MASK 0x001f
 #define GPIO_CTRL_FUNCSEL_RIO  0x5
 
-#define RIO_OUT(base) (base)
-#define RIO_OE(base)  (base + 0x4)
-#define RIO_IN(base)  (base + 0x8)
+#define RIO_OUT(port) (DEVICE_MMIO_NAMED_GET(port, rio) + 0x0)
+#define RIO_OE(port)  (DEVICE_MMIO_NAMED_GET(port, rio) + 0x4)
+#define RIO_IN(port)  (DEVICE_MMIO_NAMED_GET(port, rio) + 0x8)
 
 #define RIO_SET 0x2000
 #define RIO_CLR 0x3000
 
-#define RIO_OUT_SET(base) (RIO_OUT(base) + RIO_SET)
-#define RIO_OUT_CLR(base) (RIO_OUT(base) + RIO_CLR)
+#define RIO_OUT_SET(port) (RIO_OUT(port) + RIO_SET)
+#define RIO_OUT_CLR(port) (RIO_OUT(port) + RIO_CLR)
 
-#define RIO_OE_SET(base) (RIO_OE(base) + RIO_SET)
-#define RIO_OE_CLR(base) (RIO_OE(base) + RIO_CLR)
+#define RIO_OE_SET(port) (RIO_OE(port) + RIO_SET)
+#define RIO_OE_CLR(port) (RIO_OE(port) + RIO_CLR)
 
-#define PADS_CTRL(base, n) (base + 0x4 * (n + 1))
+#define PADS_CTRL(port, n) (DEVICE_MMIO_NAMED_GET(port, ctrl) + 0x4 * (n + 1))
 
 #define PADS_OUTPUT_DISABLE 0x80
 #define PADS_INPUT_ENABLE   0x40
@@ -48,16 +48,15 @@
 #define PADS_PULL_UP_ENABLE   0x8
 #define PADS_PULL_DOWN_ENABLE 0x4
 
-#define DEV_CFG(dev)  ((const struct gpio_rp1_config *)(dev)->config)
-#define DEV_DATA(dev) ((struct gpio_rp1_data *)(dev)->data)
+#define DEV_CFG(port)  ((const struct gpio_rp1_config *)(port)->config)
+#define DEV_DATA(port) ((struct gpio_rp1_data *)(port)->data)
 
 struct gpio_rp1_config {
 	struct gpio_driver_config common;
 
-	DEVICE_MMIO_NAMED_ROM(reg_base);
-	mem_addr_t gpio_offset;
-	mem_addr_t rio_offset;
-	mem_addr_t pads_offset;
+	DEVICE_MMIO_NAMED_ROM(gpio);
+	DEVICE_MMIO_NAMED_ROM(rio);
+	DEVICE_MMIO_NAMED_ROM(pads);
 
 	uint8_t ngpios;
 };
@@ -65,10 +64,9 @@ struct gpio_rp1_config {
 struct gpio_rp1_data {
 	struct gpio_driver_data common;
 
-	DEVICE_MMIO_NAMED_RAM(reg_base);
-	mem_addr_t gpio_base;
-	mem_addr_t rio_base;
-	mem_addr_t pads_base;
+	DEVICE_MMIO_NAMED_RAM(gpio);
+	DEVICE_MMIO_NAMED_RAM(rio);
+	DEVICE_MMIO_NAMED_RAM(pads);
 };
 
 static int gpio_rp1_pin_configure(const struct device *port, gpio_pin_t pin, gpio_flags_t flags)
@@ -80,42 +78,39 @@ static int gpio_rp1_pin_configure(const struct device *port, gpio_pin_t pin, gpi
 	}
 
 	/* Let RIO handle the input/output of GPIO */
-	sys_clear_bits(GPIO_CTRL(data->gpio_base, pin), GPIO_CTRL_OEOVER_MASK);
-	sys_set_bits(GPIO_CTRL(data->gpio_base, pin), GPIO_CTRL_OEOVER_PERI);
+	sys_clear_bits(GPIO_CTRL(port, pin), GPIO_CTRL_OEOVER_MASK);
+	sys_set_bits(GPIO_CTRL(port, pin), GPIO_CTRL_OEOVER_PERI);
 
-	sys_clear_bits(GPIO_CTRL(data->gpio_base, pin), GPIO_CTRL_OUTOVER_MASK);
-	sys_set_bits(GPIO_CTRL(data->gpio_base, pin), GPIO_CTRL_OUTOVER_PERI);
+	sys_clear_bits(GPIO_CTRL(port, pin), GPIO_CTRL_OUTOVER_MASK);
+	sys_set_bits(GPIO_CTRL(port, pin), GPIO_CTRL_OUTOVER_PERI);
 
-	sys_clear_bits(GPIO_CTRL(data->gpio_base, pin), GPIO_CTRL_FUNCSEL_MASK);
-	sys_set_bits(GPIO_CTRL(data->gpio_base, pin), GPIO_CTRL_FUNCSEL_RIO);
+	sys_clear_bits(GPIO_CTRL(port, pin), GPIO_CTRL_FUNCSEL_MASK);
+	sys_set_bits(GPIO_CTRL(port, pin), GPIO_CTRL_FUNCSEL_RIO);
 
 	/* Set the direction */
 	if (flags & GPIO_OUTPUT) {
-		sys_set_bit(RIO_OE_SET(data->rio_base), pin);
-		sys_clear_bits(PADS_CTRL(data->pads_base, pin),
-			       PADS_OUTPUT_DISABLE | PADS_INPUT_ENABLE);
+		sys_set_bit(RIO_OE_SET(port), pin);
+		sys_clear_bits(PADS_CTRL(port, pin), PADS_OUTPUT_DISABLE | PADS_INPUT_ENABLE);
 
 		if (flags & GPIO_OUTPUT_INIT_HIGH) {
-			sys_set_bit(RIO_OUT_SET(data->rio_base), pin);
-			sys_clear_bit(RIO_OUT_CLR(data->rio_base), pin);
+			sys_set_bit(RIO_OUT_SET(port), pin);
+			sys_clear_bit(RIO_OUT_CLR(port), pin);
 		} else if (flags & GPIO_OUTPUT_INIT_LOW) {
-			sys_set_bit(RIO_OUT_CLR(data->rio_base), pin);
-			sys_clear_bit(RIO_OUT_SET(data->rio_base), pin);
+			sys_set_bit(RIO_OUT_CLR(port), pin);
+			sys_clear_bit(RIO_OUT_SET(port), pin);
 		}
 	} else if (flags & GPIO_INPUT) {
-		sys_set_bit(RIO_OE_CLR(data->rio_base), pin);
-		sys_set_bits(PADS_CTRL(data->pads_base, pin),
-			     PADS_OUTPUT_DISABLE | PADS_INPUT_ENABLE);
+		sys_set_bit(RIO_OE_CLR(port), pin);
+		sys_set_bits(PADS_CTRL(port, pin), PADS_OUTPUT_DISABLE | PADS_INPUT_ENABLE);
 	}
 
 	/* Set pull up/down */
-	sys_clear_bits(PADS_CTRL(data->pads_base, pin),
-		       PADS_PULL_UP_ENABLE | PADS_PULL_DOWN_ENABLE);
+	sys_clear_bits(PADS_CTRL(port, pin), PADS_PULL_UP_ENABLE | PADS_PULL_DOWN_ENABLE);
 
 	if (flags & GPIO_PULL_UP) {
-		sys_set_bits(PADS_CTRL(data->pads_base, pin), PADS_PULL_UP_ENABLE);
+		sys_set_bits(PADS_CTRL(port, pin), PADS_PULL_UP_ENABLE);
 	} else if (flags & GPIO_PULL_DOWN) {
-		sys_set_bits(PADS_CTRL(data->pads_base, pin), PADS_PULL_DOWN_ENABLE);
+		sys_set_bits(PADS_CTRL(port, pin), PADS_PULL_DOWN_ENABLE);
 	}
 
 	return 0;
@@ -125,7 +120,7 @@ static int gpio_rp1_port_get_raw(const struct device *port, gpio_port_value_t *v
 {
 	struct gpio_rp1_data *data = port->data;
 
-	*value = sys_read32(RIO_IN(data->rio_base));
+	*value = sys_read32(RIO_IN(port));
 
 	return 0;
 }
@@ -135,11 +130,11 @@ static int gpio_rp1_port_set_masked_raw(const struct device *port, gpio_port_pin
 {
 	struct gpio_rp1_data *data = port->data;
 
-	sys_clear_bits(RIO_OUT_SET(data->rio_base), mask);
-	sys_set_bits(RIO_OUT_CLR(data->rio_base), mask);
+	sys_clear_bits(RIO_OUT_SET(port), mask);
+	sys_set_bits(RIO_OUT_CLR(port), mask);
 
-	sys_clear_bits(RIO_OUT_CLR(data->rio_base), (value & mask));
-	sys_set_bits(RIO_OUT_SET(data->rio_base), (value & mask));
+	sys_clear_bits(RIO_OUT_CLR(port), (value & mask));
+	sys_set_bits(RIO_OUT_SET(port), (value & mask));
 
 	return 0;
 }
@@ -148,8 +143,8 @@ static int gpio_rp1_port_set_bits_raw(const struct device *port, gpio_port_pins_
 {
 	struct gpio_rp1_data *data = port->data;
 
-	sys_clear_bits(RIO_OUT_CLR(data->rio_base), pins);
-	sys_set_bits(RIO_OUT_SET(data->rio_base), pins);
+	sys_clear_bits(RIO_OUT_CLR(port), pins);
+	sys_set_bits(RIO_OUT_SET(port), pins);
 
 	return 0;
 }
@@ -158,8 +153,8 @@ static int gpio_rp1_port_clear_bits_raw(const struct device *port, gpio_port_pin
 {
 	struct gpio_rp1_data *data = port->data;
 
-	sys_clear_bits(RIO_OUT_SET(data->rio_base), pins);
-	sys_set_bits(RIO_OUT_CLR(data->rio_base), pins);
+	sys_clear_bits(RIO_OUT_SET(port), pins);
+	sys_set_bits(RIO_OUT_CLR(port), pins);
 
 	return 0;
 }
@@ -169,15 +164,15 @@ static int gpio_rp1_port_toggle_bits(const struct device *port, gpio_port_pins_t
 	struct gpio_rp1_data *data = port->data;
 	uint32_t val;
 
-	val = sys_read32(RIO_OUT(data->rio_base));
+	val = sys_read32(RIO_OUT(port));
 
 	/* Low to high */
-	sys_set_bits(RIO_OUT_SET(data->rio_base), val ^ pins);
-	sys_clear_bits(RIO_OUT_CLR(data->rio_base), val ^ pins);
+	sys_set_bits(RIO_OUT_SET(port), val ^ pins);
+	sys_clear_bits(RIO_OUT_CLR(port), val ^ pins);
 
 	/* High to low */
-	sys_set_bits(RIO_OUT_CLR(data->rio_base), val & pins);
-	sys_clear_bits(RIO_OUT_SET(data->rio_base), val & pins);
+	sys_set_bits(RIO_OUT_CLR(port), val & pins);
+	sys_clear_bits(RIO_OUT_SET(port), val & pins);
 
 	return 0;
 }
@@ -193,13 +188,9 @@ static DEVICE_API(gpio, gpio_rp1_api) = {
 
 static int gpio_rp1_init(const struct device *port)
 {
-	const struct gpio_rp1_config *config = port->config;
-	struct gpio_rp1_data *data = port->data;
-
-	DEVICE_MMIO_NAMED_MAP(port, reg_base, K_MEM_CACHE_NONE);
-	data->gpio_base = DEVICE_MMIO_NAMED_GET(port, reg_base) + config->gpio_offset;
-	data->rio_base = DEVICE_MMIO_NAMED_GET(port, reg_base) + config->rio_offset;
-	data->pads_base = DEVICE_MMIO_NAMED_GET(port, reg_base) + config->pads_offset;
+	DEVICE_MMIO_NAMED_MAP(port, gpio, K_MEM_CACHE_NONE);
+	DEVICE_MMIO_NAMED_MAP(port, rio, K_MEM_CACHE_NONE);
+	DEVICE_MMIO_NAMED_MAP(port, pads, K_MEM_CACHE_NONE);
 
 	return 0;
 }
@@ -209,10 +200,12 @@ static int gpio_rp1_init(const struct device *port)
                                                                                                    \
 	static const struct gpio_rp1_config gpio_rp1_cfg_##n = {                                   \
 		.common = {.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_DT_INST(0)},                   \
-		DEVICE_MMIO_NAMED_ROM_INIT(reg_base, DT_INST_PARENT(n)),                           \
-		.gpio_offset = DT_INST_REG_ADDR_BY_IDX(n, 0),                                      \
-		.rio_offset = DT_INST_REG_ADDR_BY_IDX(n, 1),                                       \
-		.pads_offset = DT_INST_REG_ADDR_BY_IDX(n, 2),                                      \
+		DEVICE_MMIO_NAMED_ROM_INIT_BY_NAME_WITH_OFFSET(gpio, DT_INST_PARENT(n),            \
+							       DT_INST_REG_ADDR_BY_IDX(n, 0)),     \
+		DEVICE_MMIO_NAMED_ROM_INIT_BY_NAME_WITH_OFFSET(rio, DT_INST_PARENT(n),             \
+							       DT_INST_REG_ADDR_BY_IDX(n, 1)),     \
+		DEVICE_MMIO_NAMED_ROM_INIT_BY_NAME_WITH_OFFSET(pads, DT_INST_PARENT(n),            \
+							       DT_INST_REG_ADDR_BY_IDX(n, 2)),     \
 		.ngpios = DT_INST_PROP(n, ngpios),                                                 \
 	};                                                                                         \
                                                                                                    \
